fc8db378f9722d94a9df14aca09123f26a938ec5
daver <daver@LAPTOP-B7QQDHFN.lan>
2021-05-03 00:28:23 +0200

    New

--- tCHu/src/ch/epfl/tchu/gui/ObservableGameState.java
+++ tCHu/src/ch/epfl/tchu/gui/ObservableGameState.java
@@ -1,5 +1,42 @@
 package ch.epfl.tchu.gui;
 
-public class ObservableGameState {
+import java.util.List;
 
+import ch.epfl.tchu.game.Card;
+import ch.epfl.tchu.game.ChMap;
+import ch.epfl.tchu.game.PlayerId;
+import ch.epfl.tchu.game.PlayerState;
+import ch.epfl.tchu.game.PublicGameState;
+import javafx.beans.property.*;
+
+public class ObservableGameState {
+    private  PublicGameState ogs;
+    private PlayerState ops;
+    // Group 1
+    private final ReadOnlyIntegerProperty ticketPercentage; 
+    private final ReadOnlyIntegerProperty cardPercentage;
+    private final List<ReadOnlyObjectProperty<Card>> faceUpCards;
+    private final List<ReadOnlyObjectProperty<PlayerId>> routes;
+    
+    // Group 2
+    private final ReadOnlyIntegerProperty ticketsP1;
+    private final ReadOnlyIntegerProperty ticketsP2;
+    private final ReadOnlyIntegerProperty cardsP1;
+    private final ReadOnlyIntegerProperty cardsP2;
+    private final ReadOnlyIntegerProperty wagonsP1;
+    private final ReadOnlyIntegerProperty wagonsP2;
+    
+    
+    
+    
+    ObservableGameState(PlayerId id){
+        
+    }
+    
+    public void setState(PublicGameState ogs, PlayerState ops) {
+       this.ogs = ogs;
+       this.ops = ops;
+    }
+    
+    
 }
