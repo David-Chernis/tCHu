bef949ecfcbf932a73d4db25438b2d102331c451
daver <daver@LAPTOP-B7QQDHFN.lan>
2021-05-13 22:38:09 +0200

    New

--- tCHu/src/ch/epfl/tchu/gui/InfoViewCreator.java
+++ tCHu/src/ch/epfl/tchu/gui/InfoViewCreator.java
@@ -1,5 +1,12 @@
 package ch.epfl.tchu.gui;
 
+import java.util.Map;
+
+import ch.epfl.tchu.game.PlayerId;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.StringExpression;
+import javafx.collections.ObservableList;
+import javafx.geometry.Orientation;
 import javafx.scene.control.Separator;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
@@ -11,7 +18,7 @@ import javafx.scene.text.TextFlow;
  * @author Shrey Mittal (312275)
  * @author David Chernis (310298)
  */
-public class InfoViewCreator {
+class InfoViewCreator {
 	
 	/**
 	 * Private InfoViewCreator constructor, whose sole aim is to make this class uninstanciable.
@@ -23,7 +30,7 @@ public class InfoViewCreator {
 	 * @param ogs (ObservableGameState): the observable game state of the game.
 	 * @return (VBox): a graphical view of the infos of the players.
 	 */
-	public static VBox createInfoView(ObservableGameState ogs) {
+	public static VBox createInfoView(ObservableGameState ogs, PlayerId id, Map<PlayerId, String> playerNames, ObservableList<Text> infos) {
 		
 		//The circles that represents the color of each player
 		Circle cBlue = new Circle(5);
@@ -32,14 +39,21 @@ public class InfoViewCreator {
 		cRed.getStyleClass().add("filled");
 		
 		//The text part of the player statistics
-		Text textBlue = new Text(String.format(StringsFr.PLAYER_STATS, playername, playertickets, playercards, playerwagons, playerpoints));
-		Text textRed = new Text(String.format(StringsFr.PLAYER_STATS, playername, playertickets, playercards, playerwagons, playerpoints));
+		Text textBlue = new Text();
+		Text textRed = new Text();
+		
+		StringExpression sBlue = Bindings.format(StringsFr.PLAYER_STATS, playerNames.get(id), ogs.playerTickets(id), ogs.playerCards(id), ogs.playerWagons(id), ogs.playerClaimPoints(id));
+		StringExpression sRed = Bindings.format(StringsFr.PLAYER_STATS, playerNames.get(id.next()), ogs.playerTickets(id.next()), ogs.playerCards(id.next()), ogs.playerWagons(id.next()), ogs.playerClaimPoints(id.next()));
+		
+		textBlue.textProperty().bind(sBlue);
+		textRed.textProperty().bind(sRed);
 		
 		//The TextFlow of each player
 		TextFlow textFlowBlue = new TextFlow(cBlue, textBlue);
-		textFlowBlue.getStyleClass().add("PLAYER_n");
+		
+		textFlowBlue.getStyleClass().add(id == PlayerId.PLAYER_1 ? "PLAYER_1" : "PLAYER_2");
 		TextFlow textFlowRed = new TextFlow(cRed, textRed);
-		textFlowRed.getStyleClass().add("PLAYER_n");
+		textFlowRed.getStyleClass().add(id.next() == PlayerId.PLAYER_1 ? "PLAYER_1" : "PLAYER_2");
 		
 		//The VBox containing the player statistics
 		VBox playerStats = new VBox(textFlowBlue, textFlowRed);
@@ -48,20 +62,17 @@ public class InfoViewCreator {
 		//The separator that separates the player statistics and the last 5 game infos
 		Separator separator = new Separator();
 		
+		separator.orientationProperty().set(Orientation.HORIZONTAL);
+		
 		//The last 5 game infos
-		Text textLastGI = new Text();
-		Text textSecondLastGI = new Text();
-		Text textThirdLastGI = new Text();
-		Text textFourthLastGI = new Text();
-		Text textFifthLastGI = new Text();
 		
 		//The TextFlow for the last 5 game infos
-		TextFlow lastFiveGameInfos = new TextFlow(textLastGI, textSecondLastGI, textThirdLastGI,
-														textFourthLastGI, textFifthLastGI);
-		lastFiveGameInfos.setId("game-info");
+		TextFlow messages = new TextFlow();
+		Bindings.bindContent(messages.getChildren(), infos);
+		messages.setId("game-info");
 		
 		//The final Info View VBox, containing all the elements described above
-		VBox infoView = new VBox(playerStats, separator, lastFiveGameInfos);
+		VBox infoView = new VBox(playerStats, separator, messages);
 		infoView.getStylesheets().addAll("info.css", "colors.css");
 		
 		return infoView;
