41b39fa0c953657c696aa2cf784f092b6f2af7f4
daver <daver@LAPTOP-B7QQDHFN.lan>
2021-05-27 21:21:49 +0200

    Efficiency Improvement for remotePlayerProxy

--- tCHu/src/ch/epfl/tchu/net/RemotePlayerProxy.java
+++ tCHu/src/ch/epfl/tchu/net/RemotePlayerProxy.java
@@ -21,10 +21,13 @@ import ch.epfl.tchu.game.Ticket;
  */
 public final class RemotePlayerProxy implements Player{
 
-	/**
-	 * (Socket): the socket used to connect the client and the proxy.
-	 */
+    /**
+     * (Socket): the socket used to connect the client and the proxy.
+     */
     private Socket socket;
+    private BufferedReader reader;
+    private BufferedWriter writer;
+
 
     /**
      * Default public RemotePlayerProxy constructor. Initializes an instance using the Socket provided
@@ -33,6 +36,13 @@ public final class RemotePlayerProxy implements Player{
      */
     public RemotePlayerProxy(Socket socket) {
         this.socket = socket;
+        try {
+            this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), US_ASCII));
+            this.writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), US_ASCII));
+        } catch(IOException e) {
+            throw new UncheckedIOException(e);
+        }
+
     }
 
     @Override
@@ -40,85 +50,83 @@ public final class RemotePlayerProxy implements Player{
         String toBeSent = MessageId.INIT_PLAYERS.name() + " " + Serdes.playerIdSerde.serialize(ownId)
         + " " + Serdes.stringListSerde.serialize(List.of(playerNames.get(PlayerId.PLAYER_1),
                 playerNames.get(PlayerId.PLAYER_2)));
-        this.sendThroughSocket(toBeSent);
+        this.sendThroughSocket(writer, toBeSent);
     }
 
     @Override
     public void receiveInfo(String info) {
         String toBeSent = MessageId.RECEIVE_INFO.name() + " " + Serdes.stringSerde.serialize(info);
-        this.sendThroughSocket(toBeSent);
+        this.sendThroughSocket(writer, toBeSent);
     }
 
     @Override
     public void updateState(PublicGameState newState, PlayerState ownState) {
         String toBeSent = MessageId.UPDATE_STATE.name() + " " + Serdes.publicGameStateSerde.serialize(newState) 
         + " " + Serdes.playerStateSerde.serialize(ownState);
-        this.sendThroughSocket(toBeSent);
+        this.sendThroughSocket(writer, toBeSent);
     }
 
     @Override
     public void setInitialTicketChoice(SortedBag<Ticket> tickets) {
         String toBeSent = MessageId.SET_INITIAL_TICKETS.name() + " " + Serdes.ticketBagSerde.serialize(tickets);
-        this.sendThroughSocket(toBeSent);
+        this.sendThroughSocket(writer, toBeSent);
     }
 
     @Override
     public SortedBag<Ticket> chooseInitialTickets() {
         String toBeSent = MessageId.CHOOSE_INITIAL_TICKETS.name();
-        this.sendThroughSocket(toBeSent);
-        return Serdes.ticketBagSerde.deserialize(this.receiveThroughSocket());
+        this.sendThroughSocket(writer, toBeSent);
+        return Serdes.ticketBagSerde.deserialize(this.receiveThroughSocket(reader));
     }
 
     @Override
     public TurnKind nextTurn() {
         String toBeSent = MessageId.NEXT_TURN.name();
-        this.sendThroughSocket(toBeSent);
-        return Serdes.turnKindSerde.deserialize(this.receiveThroughSocket());
+        this.sendThroughSocket(writer, toBeSent);
+        return Serdes.turnKindSerde.deserialize(this.receiveThroughSocket(reader));
     }
 
     @Override
     public SortedBag<Ticket> chooseTickets(SortedBag<Ticket> options) {
         String toBeSent = MessageId.CHOOSE_TICKETS.name() + " " + Serdes.ticketBagSerde.serialize(options);
-        this.sendThroughSocket(toBeSent);
-        return Serdes.ticketBagSerde.deserialize(this.receiveThroughSocket());
+        this.sendThroughSocket(writer, toBeSent);
+        return Serdes.ticketBagSerde.deserialize(this.receiveThroughSocket(reader));
     }
 
     @Override
     public int drawSlot() {
         String toBeSent = MessageId.DRAW_SLOT.name();
-        this.sendThroughSocket(toBeSent);
-        return Serdes.intSerde.deserialize(this.receiveThroughSocket());
+        this.sendThroughSocket(writer, toBeSent);
+        return Serdes.intSerde.deserialize(this.receiveThroughSocket(reader));
     }
 
     @Override
     public Route claimedRoute() {
         String toBeSent = MessageId.ROUTE.name();
-        this.sendThroughSocket(toBeSent);
-        return Serdes.routeSerde.deserialize(this.receiveThroughSocket());
+        this.sendThroughSocket(writer, toBeSent);
+        return Serdes.routeSerde.deserialize(this.receiveThroughSocket(reader));
     }
 
     @Override
     public SortedBag<Card> initialClaimCards() {
         String toBeSent = MessageId.CARDS.name();
-        this.sendThroughSocket(toBeSent);
-        return Serdes.cardBagSerde.deserialize(this.receiveThroughSocket());
+        this.sendThroughSocket(writer, toBeSent);
+        return Serdes.cardBagSerde.deserialize(this.receiveThroughSocket(reader));
     }
 
     @Override
     public SortedBag<Card> chooseAdditionalCards(List<SortedBag<Card>> options) {
         String toBeSent = MessageId.CHOOSE_ADDITIONAL_CARDS.name() + " " + Serdes.cardListBagSerde.serialize(options);
-        this.sendThroughSocket(toBeSent);
-        return Serdes.cardBagSerde.deserialize(this.receiveThroughSocket());
+        this.sendThroughSocket(writer, toBeSent);
+        return Serdes.cardBagSerde.deserialize(this.receiveThroughSocket(reader));
     }
 
     /**
      * Returns the String that it receives from the socket connection.
      * @return (String): the String that it received from the socket connection.
      */
-    private String receiveThroughSocket() {
-        try{
-            BufferedReader reader = 
-                    new BufferedReader(new InputStreamReader(socket.getInputStream(), US_ASCII));
+    private String receiveThroughSocket(BufferedReader reader) {
+        try{     
             return reader.readLine();
         }
         catch(IOException e) {
@@ -130,10 +138,8 @@ public final class RemotePlayerProxy implements Player{
      * Sends the given String through the socket connection to the remote player.
      * @param toBeSent (String): the String to be sent.
      */
-    private void sendThroughSocket(String toBeSent) {
+    private void sendThroughSocket(BufferedWriter writer, String toBeSent) {
         try{
-            BufferedWriter writer =
-                    new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), US_ASCII));
             writer.write(toBeSent + " " + "\n");
             writer.flush();
         }
