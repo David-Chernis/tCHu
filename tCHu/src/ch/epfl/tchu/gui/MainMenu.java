3d080435a23c7d49f9610e40e32b804727f96ed7
shrey <shrey@LAPTOP-H16FT8PB.home>
2021-05-31 21:54:03 +0200

    Hola

---
+++ tCHu/src/ch/epfl/tchu/gui/MainMenu.java
@@ -0,0 +1,49 @@
+package ch.epfl.tchu.gui;
+
+import ch.epfl.tchu.game.Constants;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+
+public final class MainMenu extends Application{
+	
+	public MainMenu(Stage primStage) {
+		Text mainMenuIntroText = new Text("Choissisez le nombre des joueurs qui vont jouer !");
+		mainMenuIntroText.setTextAlignment(TextAlignment.CENTER);
+		
+		Button twoPlayerButton = new Button("2 Joueurs");
+		Button threePlayerButton = new Button("3 Joueurs");
+		HBox buttonBox = new HBox(twoPlayerButton, threePlayerButton);
+		
+		BorderPane mainMenuPane = new BorderPane(buttonBox, mainMenuIntroText, null, null, null);
+		Scene mainMenuScene = new Scene(mainMenuPane);
+		Stage mainMenuStage = primStage;
+		
+		mainMenuStage.setScene(mainMenuScene);
+		mainMenuStage.setTitle("tCHu - Main Menu");
+		mainMenuStage.show();
+		
+		twoPlayerButton.setOnMouseClicked((e) -> {
+			Constants.THREE_PLAYER = false;
+			twoPlayerButton.disableProperty().set(true);
+			buttonBox.setVisible(false);
+			mainMenuIntroText.setText("Attente de la connexion de joueur !");
+		});
+		threePlayerButton.setOnMouseClicked((e) -> {
+			Constants.THREE_PLAYER = true;
+			threePlayerButton.disableProperty().set(true);;
+			buttonBox.setVisible(false);
+			mainMenuIntroText.setText("Attente de la connexion des joueurs !");
+		});
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		new MainMenu(primaryStage);
+	}
+}
--- tCHu/src/ch/epfl/tchu/gui/ServerMain.java
+++ tCHu/src/ch/epfl/tchu/gui/ServerMain.java
@@ -10,6 +10,7 @@ import java.util.Random;
 
 import ch.epfl.tchu.SortedBag;
 import ch.epfl.tchu.game.ChMap;
+import ch.epfl.tchu.game.Constants;
 import ch.epfl.tchu.game.Game;
 import ch.epfl.tchu.game.Player;
 import ch.epfl.tchu.game.PlayerId;
@@ -18,6 +19,7 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import static ch.epfl.tchu.game.PlayerId.PLAYER_1;
 import static ch.epfl.tchu.game.PlayerId.PLAYER_2;
+import static ch.epfl.tchu.game.PlayerId.PLAYER_3;
 
 /**
  * Class that represents the server that manages communication between the client and proxy player on which tchu is being played.  
@@ -37,29 +39,64 @@ public class ServerMain extends Application{
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Setting up Server
-        RemotePlayerProxy playerProxy = null;
-        GraphicalPlayerAdapter gpa = new GraphicalPlayerAdapter();
-        
-        try{
-            ServerSocket server = new ServerSocket(5108);
-            Socket s = server.accept();
-            playerProxy = new RemotePlayerProxy(s);
-        } catch(IOException e) {
-            throw new UncheckedIOException(e);
-        }
-        
-        List<String> names = getParameters().getRaw();
-        Random rand = new Random();
-        
-        //Initializaing Player Maps and GraphicalPlayerAdapter through analyzing parameters
-        Map<PlayerId, String> playerNames = Map.of( 
-                PLAYER_1, names.isEmpty() ? "Ada" : names.get(0),  
-                PLAYER_2, names.isEmpty() ? "Charles" : names.get(1)); 
-        Map<PlayerId, Player> players = Map.of( 
-                PLAYER_1, gpa,
-                PLAYER_2, playerProxy);
-        
-        new Thread(() -> Game.play(players, playerNames, SortedBag.of(ChMap.tickets()), rand)).start();
+    	//Creates main menu
+    	new MainMenu(primaryStage);
+    	if(Constants.THREE_PLAYER) {
+    		// Setting up Server
+	        RemotePlayerProxy playerProxy1 = null;
+	        RemotePlayerProxy playerProxy2 = null;
+	        GraphicalPlayerAdapter gpa = new GraphicalPlayerAdapter();
+	        
+	        try{
+	            ServerSocket server = new ServerSocket(5108);
+	            Socket s1 = server.accept();
+	            Socket s2 = server.accept();
+	            playerProxy1 = new RemotePlayerProxy(s1);
+	            playerProxy2 = new RemotePlayerProxy(s2);
+	        } catch(IOException e) {
+	            throw new UncheckedIOException(e);
+	        }
+	        
+	        List<String> names = getParameters().getRaw();
+	        Random rand = new Random();
+	        
+	        //Initializaing Player Maps and GraphicalPlayerAdapter through analyzing parameters
+	        Map<PlayerId, String> playerNames = Map.of( 
+	                PLAYER_1, names.isEmpty() ? "Ada" : names.get(0),  
+	                PLAYER_2, names.isEmpty() ? "Charles" : names.get(1),
+	        		PLAYER_3, names.isEmpty() ? "Michel" : names.get(2)); 
+	        Map<PlayerId, Player> players = Map.of( 
+	                PLAYER_1, gpa,
+	                PLAYER_2, playerProxy1,
+	                PLAYER_3, playerProxy2);
+	        
+	        new Thread(() -> Game.play(players, playerNames, SortedBag.of(ChMap.tickets()), rand)).start();
+    	}
+    	else {
+	        // Setting up Server
+	        RemotePlayerProxy playerProxy = null;
+	        GraphicalPlayerAdapter gpa = new GraphicalPlayerAdapter();
+	        
+	        try{
+	            ServerSocket server = new ServerSocket(5108);
+	            Socket s = server.accept();
+	            playerProxy = new RemotePlayerProxy(s);
+	        } catch(IOException e) {
+	            throw new UncheckedIOException(e);
+	        }
+	        
+	        List<String> names = getParameters().getRaw();
+	        Random rand = new Random();
+	        
+	        //Initializaing Player Maps and GraphicalPlayerAdapter through analyzing parameters
+	        Map<PlayerId, String> playerNames = Map.of( 
+	                PLAYER_1, names.isEmpty() ? "Ada" : names.get(0),  
+	                PLAYER_2, names.isEmpty() ? "Charles" : names.get(1)); 
+	        Map<PlayerId, Player> players = Map.of( 
+	                PLAYER_1, gpa,
+	                PLAYER_2, playerProxy);
+	        
+	        new Thread(() -> Game.play(players, playerNames, SortedBag.of(ChMap.tickets()), rand)).start();
+    	}
     }
 }
